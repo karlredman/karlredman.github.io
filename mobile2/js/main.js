cf7f1e99c2c3885b1d384a1a21439c928eed80f9
Karl Redman <karl.redman@gmail.com>
2016-03-19 22:19:47 -0500

    regular update

---
+++ mobile2/js/main.js
@@ -0,0 +1,16 @@
+
+/*
+ *$(function() {
+ *    alert( 'JavaScript Loaded!' );
+ *});
+ */
+
+/*
+ *(function($) {
+ *  if ($(window).width() > 400 ) {
+ *    $("input[type='checkbox']").attr("checked", true);
+ *    }
+ *    
+ *});
+ */
+
